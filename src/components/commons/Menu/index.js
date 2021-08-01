66b1b4b73d01f0c6cc4d2eacd49aacceb62cf80f
Ricardo de Almeida Paula <ricardo.paula@mpce.mp.br>
2021-08-01 10:16:54 -0300

    Aula 05

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MenuWrapper } from './styles/MenuWrapper';
-import { Logo } from '../../theme/Logo/index';
+import { Logo } from '../../../theme/Logo/index';
 import { Button } from '../Button';
 
 export default function Menu() {
